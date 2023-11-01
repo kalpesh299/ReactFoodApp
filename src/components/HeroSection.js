6ac038626a8d9ce6b6dd42f40a9cf554ed93ce9b
Kalpesh Patil <72344646+kalpesh299@users.noreply.github.com>
2023-11-01 17:26:02 +0530

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const HeroSection = () => {
   return (
-     <div>
-     <section class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1770&amp;q=80)] bg-cover bg-center bg-no-repeat">
+     <div className='bg-slate-300'>
+     <section class="relative bg-red-200 bg-cover bg-center bg-no-repeat">
         <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l">
             </div><div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
                 <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
